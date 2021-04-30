adf14aa328b138dfc4498b5d28cc66517389952b
shibafu <civaf-th123@hotmail.co.jp>
2021-04-30 12:43:23 +0900

    メニュー作成

---
+++ memFrame/static/memFrame/js/default.js
@@ -0,0 +1,13 @@
+// メニュー読み込み
+
+
+  $(function() {
+//  alert("hello");
+//$(".menu_item").animate({
+//    height : "toggle"
+//});
+  // メニュー表示
+//   $(".menu_btn").click(function() {
+//       $(".menu_item").show();
+//   });
+});
